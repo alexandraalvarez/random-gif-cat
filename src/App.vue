006c0592927b50b8a1a4347cacefdaae2196e879
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2020-11-01 23:57:44 -0300

    revisar imagen que no se despliega

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app" class="my-5">
+ 
+    <randomcat></randomcat>
+    
+</div>
 </template>
 
-<style lang="scss">
+<script>
+import randomcat from './components/RandomCat.vue'
+export default {
+    data() {
+        return {};
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    components: {
+        'randomcat': randomcat,
+    },
+
+}
+</script>
+
+<style>
+html{
+  background-color: lightblue;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: lightblue;
 }
 </style>
---
+++ src/components/RandomCat.vue
@@ -0,0 +1,164 @@
+<template>
+
+    <div class="row d-flex justify-content-center my-0 mx-0 ">
+        <div class="col-md-12 colNotPink">
+            <h1 class="text-center mt-5 mb-5"> Random gif cat</h1>
+        </div>
+        <div class="col-md-6">
+            <div id="form">
+                <div id="card">
+                    <div class="form-group pt-3">
+                        <label>Titulo:</label>
+                        <input type="text" class="form-control" v-model="tittle" ref="titulo">
+                    </div>
+                    <div class="form-group">
+                        <label>Filtro:</label>
+                        <select id="inputState" class="custom-select" @change="changeFilter($event)">
+                            <option v-for="opt in filtros" :key="opt" :value="opt" v-text="opt"></option>
+                        </select>
+                    </div>
+                    <div class="form-group ">
+                        <label>Color:</label>
+                        <div class="input-group">
+                            <select id="inputState" class="custom-select" @change="changeColor($event)">
+                                <option v-for="color in colors" :key="color.color" :value="color.value" v-text="color.color"></option>
+                            </select>
+                            <div class="input-group-prepend ml-3 color-redondeado input-group-text color-show" :style="style">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group pb-3">
+                        <label >Tamaño (px):</label>
+                        <input type="text" class="form-control" v-model="size" ref="size">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 colNotPink">
+            <a href="#" class="btn btn-lg" @click="GetMyCat">¡Quiero mi gatito!</a>
+            <img :src="url" class="img-fluid d-block mt-5 mb-5">
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'randomcat',
+    // props: {},
+    data: function () {
+        return {
+            tittle: "",
+            size: "",
+            myFilter: {
+            filter: "",
+            },
+            filtros: [
+                "blur",
+                "mono",
+                "negative",
+                "paint",
+                "sepia",
+            ],
+            selected: "",
+            url: "",
+            colors: [{
+                    color: "Blanco",
+                    value: "white"
+                },
+                {
+                    color: "Verde",
+                    value: "green"
+                },
+                {
+                    color: "Rojo",
+                    value: "red"
+                },
+                {
+                    color: "Azul",
+                    value: "blue"
+                },
+                {
+                    color: "Amarillo",
+                    value: "yellow"
+                },
+                {
+                    color: "Morado",
+                    value: "purple"
+                }
+
+            ],
+            style: {
+                background: ""
+            }
+        }
+    },
+    // computed: {},
+    methods: {
+        changeFilter(e) {
+            var response = e.target.value
+            if (response === "blur") {
+                this.selected = "blur"
+
+                console.log(this.selected)
+            } else if (response === "mono") {
+                this.selected = "mono"
+
+            } else if (response === "negative") {
+                this.selected = "negative"
+
+            } else if (response === "paint") {
+                this.selected = "paint"
+
+            } else if (response === "sepia") {
+                this.selected = "sepia"
+
+            }
+            return
+            selected, url
+        },
+        changeColor(e) {
+            this.style.background = e.target.value
+        },
+        GetMyCat() {
+            if (this.tittle === "") {
+                alert("Escribe un mensaje.")
+                this.$refs.titulo.focus()
+            }
+             else if (this.size > 400 || this.size < 1 || isNaN(this.size)) {
+                alert("Ingrese un número entre 1 y 400")
+                this.$refs.size.value = ""
+                this.$refs.size.focus()
+            } else
+                this.url = `https://cataas.com/cat/gif/says/${this.tittle}?filter=${this.selected}&color=${this.style.background}&size=${this.size}&type=or`
+        }
+    },
+    // components: {},
+}
+</script>
+
+<style>
+
+.row{
+  width: 100%;
+  display: inline-flex;
+  box-sizing: border-box;
+  background-color: lightcoral;
+  padding: 0;
+}
+
+.colNotPink {
+    background-color: lightblue;
+}
+
+.color-redondeado {
+  height: 30px;
+  border-radius: 15px;
+  width: 30px;
+  border: 1px solid lightgray;
+}
+
+.btn {
+    background-color: white;
+}
+</style>
